8ebe453efdc31e6b97ae4e88b442935d345febcd
myklory <xyzaabb11@163.com>
2016-08-18 17:24:33 +0800

    增加7天的天气查询

--- app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
@@ -1,22 +1,194 @@
 package com.example.android.sunshine.app;
 
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.util.Log;
+import android.view.*;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
+    ArrayAdapter<String> foreCastAdapter;
+
     public MainActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //告诉fragment需要一个菜单。
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        //从菜单资源加载菜单
+        inflater.inflate(R.menu.forecastfragment, menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        //判断id是不是我们点击的菜单id
+        if (id == R.id.action_refresh){
+            new FetchWeatherTask().execute("Chengdu");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        /*ArrayList<String> foreCast = new ArrayList<String>();
+        foreCast.add("today-sunny-88/67");
+        foreCast.add("tomorrow-cloud-86/63");*/
+        new FetchWeatherTask().execute("Chengdu");
+        String[] weathers = {
+                "today-sunny-88/67",
+                "tomorrow-cloud-86/63",
+                "San-froggy-83/60",
+                "Sun-sunny-89/68"
+
+        };
+        ArrayList<String> foreCastList = new ArrayList<String>(Arrays.asList(weathers));
+
+        foreCastAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                foreCastList);
+
+        ListView listView = (ListView)rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(foreCastAdapter);
+        return rootView;
+    }
+
+
+
+    private class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+
+        /**
+         * 执行查询天气
+         * @param strings
+         * @return
+         */
+        @Override
+        protected String[] doInBackground(String... strings) {
+            String foreCastJson = getForeCast(strings[0]);
+            String[] weathers = WeatherDataParser.getWeatherDataFromJson(foreCastJson);
+            Log.i("getForeCast", weathers.toString());
+            //parseWeather(foreCastJson);
+            return weathers;
+        }
+
+        /**
+         * 得到天气结果后更新界面
+         * @param strings
+         */
+        @Override
+        protected void onPostExecute(String[] strings) {
+        //    super.onPostExecute(strings);
+            foreCastAdapter.clear();
+            foreCastAdapter.addAll(strings);
+        }
+
+        private void parseWeather(String foreCastJson){
+            double maxTemp = WeatherDataParser.getMaxTemperatureForDay(foreCastJson, 0);
+            Log.i("parseWeather", Double.toString(maxTemp));
+        }
+
+        private String getForeCast(String city) {
+            String forecastJsonStr = null;
+            BufferedReader reader = null;
+            HttpURLConnection conn = null;
+            InputStream is = null;
+
+            //设置Uri变量
+            final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily";
+            final String QUERY_PARAM = "q";
+            final String FORMAT_PARAM = "mode";
+            final String UNITS_PARAM = "units";
+            final String DAYS_PARAM = "cnt";
+            final String APP_ID_PARAM = "appid";
+
+            //设置Uri参数
+            String mode = "json";
+            String units = "metric";
+            Integer numDays = 7;
+            String appid = "540472accf47d8ee6ee6d8955d638205";
+
+            //通过变量和参数构造Uri
+            Uri uriBuilder = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                    .appendQueryParameter(QUERY_PARAM, city)
+                    .appendQueryParameter(FORMAT_PARAM, mode)
+                    .appendQueryParameter(UNITS_PARAM, units)
+                    .appendQueryParameter(DAYS_PARAM, numDays.toString())
+                    .appendQueryParameter(APP_ID_PARAM, appid).build();
+
+            try{
+                URL url = new URL(uriBuilder.toString());
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.connect();
+                is = conn.getInputStream();
+
+                StringBuffer buffer = new StringBuffer();
+                reader = new BufferedReader(new InputStreamReader(is));
+                String line;
+                while ((line = reader.readLine()) != null){
+                    buffer.append(line + "\n");
+                }
+                if (buffer.length() != 0){
+                    forecastJsonStr = buffer.toString();
+                }
+            } catch (MalformedURLException e){
+                Log.e("MainActivityFragment", "getForeCast", e);
+            } catch (ProtocolException e){
+                Log.e("MainActivityFragment", "getForeCast", e);
+            } catch (IOException e) {
+                Log.e("MainActivityFragment", "getForeCast", e);
+            } catch (Exception e) {
+                Log.e("MainActivityFragment", "getForeCast", e);
+            } finally {
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        Log.e("MainActivityFragment", "getForeCast", e);
+                    }
+                }
+                if (is != null) {
+                    try {
+                        is.close();
+                    } catch (IOException e) {
+                        Log.e("MainActivityFragment", "getForeCast", e);
+                    }
+                }
+                if (conn != null) {
+                    conn.disconnect();
+                }
+            }
+
+            return forecastJsonStr;
+        }
     }
 }
---
+++ app/src/main/java/com/example/android/sunshine/app/WeatherDataParser.java
@@ -0,0 +1,102 @@
+package com.example.android.sunshine.app;
+
+import android.text.format.Time;
+import android.util.Log;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by myklory on 2016/8/18.
+ */
+public class WeatherDataParser {
+
+
+
+    /**
+     * 将timestamp转换为能识别的日期
+     * @param time
+     * @return
+     */
+    public static String getReadableDate(long time) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM dd");
+        return dateFormat.format(time);
+    }
+
+    public static String formatTemperature(double max, double min) {
+        return Math.round(max) + "/" + Math.round(min);
+    }
+
+    /**
+     * 通过json解析对应天的最高温度
+     * @param weatherJsonStr
+     * @param dayIndex
+     * @return
+     */
+    public static double getMaxTemperatureForDay(String weatherJsonStr, int dayIndex){
+        try {
+            JSONObject jsonObject = new JSONObject(weatherJsonStr);
+            JSONArray jsonArray = jsonObject.getJSONArray("list");
+            if (jsonArray.length() >= dayIndex){
+                JSONObject dayJson = jsonArray.getJSONObject(dayIndex);
+                return dayJson.getJSONObject("temp").getDouble("max");
+            }
+            return -1;
+        } catch (JSONException e) {
+            Log.e("getMaxTemperatureForDay", e.getMessage(), e);
+        }
+        return -1;
+    }
+
+    /**
+     * 从JSON中提取天气数据
+     * @param weatherJsonStr
+     * @return
+     */
+    public static String[] getWeatherDataFromJson(String weatherJsonStr){
+        String[] weathers = null;
+        final String OWM_LIST = "list";
+        final String OWM_WEATHER = "weather";
+        final String OWM_TEMP = "temp";
+        final String OWM_MAIN = "main";
+        final String OWM_MAX = "max";
+        final String OWM_MIN = "min";
+        final String OWM_DT = "dt";
+
+        //取今天日期
+        Calendar calendar = Calendar.getInstance();
+
+        try {
+            JSONObject weatherJson = new JSONObject(weatherJsonStr);
+            JSONArray daysWeather = weatherJson.getJSONArray(OWM_LIST);
+
+            weathers = new String[daysWeather.length()];
+            for (int i = 0; i < daysWeather.length(); ++i) {
+                String day;
+                String description;
+                String highAndLow;
+                JSONObject dayJson = daysWeather.getJSONObject(i);
+                //获取日期
+                //获取今天日期
+                //long dateTime =
+                day = getReadableDate(calendar.getTime().getTime());
+                calendar.add(calendar.DATE, 1);
+                //获取天气情况
+                description = dayJson.getJSONArray(OWM_WEATHER).getJSONObject(0).getString(OWM_MAIN);
+                //获取最高温度和最低温度
+                JSONObject tempJson = dayJson.getJSONObject(OWM_TEMP);
+                highAndLow = formatTemperature(tempJson.getDouble(OWM_MAX), tempJson.getDouble(OWM_MIN));
+                weathers[i] = day + " - " + description + " - " + highAndLow;
+            }
+
+        } catch (JSONException e) {
+            Log.e("getWeatherDataFromJson", e.getMessage(), e);
+        }
+        return weathers;
+    }
+
+}
